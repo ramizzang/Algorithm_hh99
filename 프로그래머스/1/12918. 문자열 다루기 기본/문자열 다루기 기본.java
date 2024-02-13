cc61946d055124f822fed5db5d2b22e37a92c8e3
ramizzang <101313500+ramizzang@users.noreply.github.com>
2024-02-13 19:57:45 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,18 +1,16 @@
 class Solution {
     public static boolean solution(String s) {
         boolean answer = true;
+        
         if(s.length() == 4 || s.length() == 6){
-            String[] strArr = s.split("");
-            for(String str : strArr){
-                if(!(str.compareTo("0") >= 0 && str.compareTo("9") <= 0)){
-                    // "1" 이상 "9" 이하의 문자열일 때의 처리
-                    return false;
+            for(int i=0; i<s.length(); i++){
+                if(!('0'<= s.charAt(i) && s.charAt(i) <= '9') ){
+                   return false;
                 }
             }
         }else{
-            return false;
+           return false;
         }
-
         return answer;
     }
 }
