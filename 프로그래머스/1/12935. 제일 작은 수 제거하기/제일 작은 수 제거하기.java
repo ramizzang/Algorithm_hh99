bebc276e92a74577e3a5ce0671751bd7ffd9e159
ramizzang <101313500+ramizzang@users.noreply.github.com>
2024-02-13 17:30:31 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.09 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+        //배열 생성
+        if (arr.length == 1) {
+            return new int[]{-1};
+        }
+        
+        int[] answer = Arrays.copyOf(arr, arr.length - 1);
+        
+        //가장 작은 수를 제거한 배열을 리턴, 빈 배열일 경우 -1 채워서 리턴
+        
+        int minIdx = 0;
+        for(int i =0; i < arr.length; i++){
+            if(arr[i] < arr[minIdx]){
+                //minIdx에 i를 저장
+                minIdx = i;
+            }
+        }
+        
+        for(int i = minIdx; i < answer.length; i++){
+            answer[i] = arr[i +1];
+        }
+        return answer;
+    }
+}
