b0bcf42133b6d54c0ccbf087232a0ed1d93ff0d4
ramizzang <101313500+ramizzang@users.noreply.github.com>
2024-02-13 14:52:56 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean solution(int x) {
+        int originX = x;
+        int sum = 0;
+        while(x>0){
+            sum +=  x%10;
+            x /= 10;
+        }
+        
+        return (originX % sum == 0) ? true : false;
+    }
+}
