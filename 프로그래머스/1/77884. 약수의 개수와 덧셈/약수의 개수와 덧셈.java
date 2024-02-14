a11cdd5dc1e6163b68a9a0af0a5a397cda897dca
ramizzang <101313500+ramizzang@users.noreply.github.com>
2024-02-14 19:17:07 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 11.14 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -1,23 +1,31 @@
 class Solution {
     public int solution(int left, int right) {
         int answer = 0;
-
-        for (int i = left; i <= right; i++) {
-            answer += measure(i) % 2 == 0 ? i : -i;
-        }
-
-        return answer; // 결과값 반환 추가
-    }
-
-    public static int measure(int number) { // 약수의 개수를 구하는 메서드
-        int result = 0;
-
-        for (int i = 1; i <= number; i++) {
-            if (number % i == 0) {
-                result++;
+        //left 부터 right 까지 모든 수들 중
+        // 약수의 개수가 짝수인 수는 더해
+        // 약수의 개수가 홀수인 수는 빼서 리턴
+        
+        //13,14,15,16,17 까지 
+        for(int i = left; i<= right; i++){//left~right 까지의 수들
+            //약수의 개수를 return 받아야 한다.
+            //i = left i 별로 count 필요
+            int count=0;
+            for(int j=1; j<=i; j++){
+                if(i % j ==0){
+                    count++;
+                }
             }
+            System.out.println(i+" : 약수개수 : "+count);
+            
+            if(count % 2 == 0){ //약수개수가 짝수
+                answer += i;
+            }else{
+                answer -= i;
+            }
+            
         }
-
-        return result;
+        
+        
+        return answer;
     }
 }
