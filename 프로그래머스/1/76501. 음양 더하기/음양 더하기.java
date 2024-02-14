3f917ee2d34796c15e0b27aa431966ecffeebbe1
ramizzang <101313500+ramizzang@users.noreply.github.com>
2024-02-14 16:43:45 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,13 +1,15 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
-        int index = 0;
-        for(int num : absolutes){
-            num = signs[index] ? +num : -num ;
-            // System.out.println(num);
-            answer += num;
-            index++;
+        // 정수들의 절댓값을 차례다로 담은 정수배열
+        // 부호를 차례대로 담은 불리언 배열 signs[i] == true 이면 양수
+        // 두 배열의 길이는 같다. 정수들의 합을 구하여라
+        
+        // 배열의 길이만큼 만복
+        for(int i =0; i < signs.length; i++){
+            answer += signs[i] ? absolutes[i] : -absolutes[i];
         }
+       
         return answer;
     }
 }
