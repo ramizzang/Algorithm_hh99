001d08bc7113c80d1edb3d0bec7eb83a4d47e3b3
ramizzang <101313500+ramizzang@users.noreply.github.com>
2024-02-14 16:47:42 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,11 +1,9 @@
 class Solution {
-    public String solution(String phone_number) {
-        int strLength =  phone_number.substring(0,phone_number.length()-4).length();
-    String star = "*".repeat(strLength);
-        String backNumber = phone_number.substring(phone_number.length()-4);
-        
-        // System.out.println((phone_number.substring(phone_number.length()-4)));
-        // System.out.println("*".repeat(phone_number.substring(0,phone_number.length()-4).length()));
-        return star+backNumber;
-    }
+  public String solution(String phone_number) {
+     char[] ch = phone_number.toCharArray();
+     for(int i = 0; i < ch.length - 4; i ++){
+         ch[i] = '*';
+     }
+     return String.valueOf(ch);
+  }
 }
