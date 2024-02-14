0dad26fb4f4bd5c6759e28c55127bdd47c4709c1
ramizzang <101313500+ramizzang@users.noreply.github.com>
2024-02-14 12:29:07 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 2.06 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,24 +1,19 @@
 import java.util.*;
 class Solution {
-     public long solution(long n) {
-            // 1. while % -> 각 자리수를
-            // 2. LongStream 으로 .char
-            // 3. String -> int -> Arr -> sort
-
-            List<Long> arr = new ArrayList<Long>();
-
-            while (n > 0) {
-                Long digit = n % 10;
-                n /= 10;
-                arr.add(digit);
-            }
-//            Collections.reverse(arr);
-            Collections.sort(arr, Collections.reverseOrder());
-
-            long answer = 0;
-            for(int i =0; i<arr.size(); i++){
-                answer = (answer * 10) + arr.get(i) ; // 인덱스 값 하나씩 가져옴
-            }
-            return answer;
+    public static long solution(long n){
+        // 각 자릿수를 배열 or list에 저장
+        // 방법 1. Strtin 으로 변환 후 정렬 한 뒤 더하기
+        // 방법 2. list or 배열에 저장 한뒤 collections를 사용 or for문을 사용하여 뒤에서부터 저장해주기
+        long answer = 0;
+        String result="";
+        //1. long 을 스트링으로 변환 하여 배열로 저장
+        String[] strNum = String.valueOf(n).split("");
+        Arrays.sort(strNum);
+        for(int i=strNum.length-1; i>=0; i--){
+            result += strNum[i];
         }
+
+
+        return Long.parseLong(result);
+    }
 }
