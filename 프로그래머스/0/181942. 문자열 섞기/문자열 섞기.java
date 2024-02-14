96bcb436ad819076917fefb7badfb7698e0007e3
ramizzang <101313500+ramizzang@users.noreply.github.com>
2024-02-14 22:21:53 +0900

    [level 0] Title: 문자열 섞기, Time: 1.36 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -2,7 +2,8 @@ class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
         for(int i = 0; i<str1.length(); i++){
-            answer += String.valueOf(str1.charAt(i)) + String.valueOf(str2.charAt(i));
+            answer += String.valueOf(str1.charAt(i));
+            answer += String.valueOf(str2.charAt(i));
         }
         
         return answer;
