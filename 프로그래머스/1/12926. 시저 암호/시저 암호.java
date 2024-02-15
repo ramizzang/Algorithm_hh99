4634aac5afd105896d28c618f1f1d5343070a59c
ramizzang <101313500+ramizzang@users.noreply.github.com>
2024-02-15 13:11:15 +0900

    [level 1] Title: 시저 암호, Time: 28.35 ms, Memory: 96.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String s, int n) {
+        String answer = "";
+        char[] chArr = s.toCharArray();
+        // AB 1만큼 밀면 BC
+        // Z 1 밀면 A
+        // 대문자,소문자, 공백
+        for(char ch : chArr){
+         if('a'<=ch && ch<='z'){ //ch가 소문자일때
+             answer += (char)(ch + n) > 'z' ? (char)(ch - 26 + n) : (char)(ch + n);
+             // ch + n > z ? ch - z + n : ch + n
+         }
+         else if ('A'<=ch && ch<='Z') { //ch가 대문자일때
+             answer += (char)(ch + n) > 'Z' ? (char)(ch - 26 + n) : (char)(ch + n);
+         }else { // 공백일때는 공백 유지
+             answer += " ";
+         }
+        }
+        return answer;
+    }
+}
