cd7dd5c8623ef49c06f68d31472f164547999ef5
ramizzang <101313500+ramizzang@users.noreply.github.com>
2024-02-16 22:25:31 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 4.75 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        //pat이 존재하면 1, 아니면 0
+        
+
+        
+        
+        return myString.toLowerCase().contains(pat.toLowerCase())?1:0;
+    }
+}
