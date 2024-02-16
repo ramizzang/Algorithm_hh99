d3d2ab8acaa95472ac7803e79e693dd04c8c64f2
ramizzang <101313500+ramizzang@users.noreply.github.com>
2024-02-16 14:25:25 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.06 ms, Memory: 66.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+
+        List<Integer> rank = new ArrayList<>();
+
+        for(int i=0; i<score.length; i++){
+            rank.add(score[i]);
+            if(rank.size()>k){ //가장 낮은 점수 제거
+                rank.remove(Collections.min(rank));
+            }
+            answer[i] = Collections.min(rank);
+        }
+        return answer;
+    }
+}
