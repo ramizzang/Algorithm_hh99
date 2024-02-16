1e8f8a6de8c5afd021c87be11eebaf68c89432b9
ramizzang <101313500+ramizzang@users.noreply.github.com>
2024-02-16 20:02:32 +0900

    [level 0] Title: 수 조작하기 1, Time: 0.99 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = 0;
+        // w +1
+        // s -1
+        // d +10
+        // a -10
+        
+        for(int i=0; i<control.length(); i++){
+            switch(control.charAt(i)){
+                case 'w':
+                    n += 1;
+                    break;
+                case 's':
+                    n -= 1;
+                    break;
+                case 'd':
+                    n += 10;
+                    break;
+                 case 'a':
+                    n -= 10;
+                    break;
+            }
+        }
+        
+        
+        return n;
+    }
+}
