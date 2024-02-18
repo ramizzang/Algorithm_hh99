687cc063abbb928c69e435044e954244d1057250
ramizzang <101313500+ramizzang@users.noreply.github.com>
2024-02-19 01:05:55 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 1.55 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        // queries s,e,k
+        // s<= i <= e
+        // i 가 k의 배수이면 arr[i] 에 1을 더한다
+        
+        for(int[] query : queries){
+            int s = query[0];
+            int e = query[1];
+            int k = query[2];
+            
+            //0~4 0,1,2,3,4 / 1 / 1의 배수 이면 arr[i]에 1d을 더한다
+            //arr[0] arr[1] arr[2] arr[3] arr[4] 1,2,3,5
+            // 0은 모든 수의 배수 i%k == 0 -> arr[i]++;
+            
+            for(int i=s; i<=e; i++){  
+                if(i==0 || i%k ==0){
+                    arr[i]++;
+                    
+                }
+            }
+                       
+            
+        }
+        
+        
+        
+        
+        return arr;
+    }
+}
