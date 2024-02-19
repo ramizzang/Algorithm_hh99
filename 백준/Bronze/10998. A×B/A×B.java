ba5857d82923dad6fa335bcf8e2db04549730bdb
ramizzang <101313500+ramizzang@users.noreply.github.com>
2024-02-19 18:55:10 +0900

    [Bronze V] Title: A×B, Time: 204 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    //두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+
+    public static void main(String[] args) {
+//        1 2
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a*b);
+    }
+}
