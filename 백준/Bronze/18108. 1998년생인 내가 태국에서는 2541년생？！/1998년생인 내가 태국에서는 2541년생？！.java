1e6ca9c7abc060d74c706c61b6c7020a78792acc
ramizzang <101313500+ramizzang@users.noreply.github.com>
2024-02-19 19:50:09 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //레드시프트 일행 태국은 2562년
+        // 2541 -> 1998
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        System.out.println(y-543);
+        sc.close();
+    }
+}
