1c4adf052ef3f95e4b4432a48bb2ad2a80bc12fd
ramizzang <101313500+ramizzang@users.noreply.github.com>
2024-02-22 09:00:34 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 212 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,23 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = sc.nextInt();
+        String stars = "";
+
+        for(int i=0; i<N; i++){
+            for (int j = 0; j <=i ; j++) {
+                stars = "*";
+                bw.write(stars);
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
