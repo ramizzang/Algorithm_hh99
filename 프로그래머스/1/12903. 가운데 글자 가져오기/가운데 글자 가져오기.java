7b30dd8ff137c4ec5fcb2c0620b19fb31d5855a3
ramizzang <101313500+ramizzang@users.noreply.github.com>
2024-03-27 09:19:22 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 1.49 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -2,16 +2,19 @@ class Solution {
     public String solution(String s) {
         String answer = "";
         String[] strArr = s.split("");
-        //단어의 길이가 짝수면 가운데 두글자 반환
-        // s 길이 1 이상 100 이하
+        
         int len = s.length();
-        if( !(len % 2 == 0)){//홀수일때
-//            char ch = s.charAt(len/2);
+        
+        if(!(s.length() %2 == 0)){
             answer = strArr[len/2];
-        }else{ //짝수는 가운데 두개 0123 2,1
-            answer = strArr[len/2-1] + strArr[len/2] ;
-            
         }
+        
+        if(s.length() %2 == 0){
+            answer =strArr[len/2-1] +strArr[len/2];
+        }
+        
+        
+        
         return answer;
     }
 }
