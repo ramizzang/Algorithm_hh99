f46b4056aaecb21269865149199e72141fcf7054
ramizzang <101313500+ramizzang@users.noreply.github.com>
2024-03-27 09:40:02 +0900

    [level 1] Title: 내적, Time: 0.06 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -1,11 +1,16 @@
 class Solution {
     public int solution(int[] a, int[] b) {
         int answer = 0;
-        //길이가 같ㅌ은 배열
-        for(int i =0; i < a.length; i++){
-            answer += a[i]*b[i];
+        //길이가 같은 두 1차우너 정수 배열 a,b
+        // 내적을 return
+        // a[n-1]*b[n-1]
+        int idx = 0;
+        for(int num: a){
+            answer += a[idx]*b[idx];
+            idx++;
         }
-
+        
+        
         return answer;
     }
 }
