eafa443d6c6cacc0522937e8b7da0ef609d41bde
ramizzang <101313500+ramizzang@users.noreply.github.com>
2024-03-27 09:33:37 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.05 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,14 +1,16 @@
 class Solution {
     public String solution(int n) {
-        String answer = "";
-        //n이 홀수이면 수박수
-        //짝수이면 수박
-        //홀일때 수 짝일때 박
+        // 길이 n 
+        // 수박수박 패턴
+        // n이 4이면 수박수박 , 3이면 수박수
+        // 0 수 1 박 2수 3박
+        // 짝 : 수 , 홀 : 박
         
-        for(int i = 1; i <= n; i++){
-            answer += i % 2 == 0? "박" : "수";
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i < n; i++){
+            sb.append(i%2==0?"수":"박");
         }
-    
-        return answer;
+   
+        return sb.toString();
     }
 }
